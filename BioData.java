3d7de5493fbd74794e01b0548204b250c4b6c3ce
aminur167 <aminurhstu23@gmail.com>
2025-10-19 09:47:42 +0600

    java

---
+++ BioData.java
@@ -0,0 +1,8 @@
+public class BioData {
+    public static void main(String[] args) {
+        System.out.println("Aminur Islam");
+        System.out.println("BSc in CSE");
+        System.out.println("Bogura");
+    }
+
+}
